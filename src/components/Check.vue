7e0a1228b018f30bc35755ba0e06f404162a3f6a
Theerawat <theerawat.a@cdg.co.th>
2021-09-30 14:57:54 +0700

    commit

--- src/components/Check.vue
+++ src/components/Check.vue
@@ -1,40 +1,100 @@
 <template>
-  <div>
-    <v-sheet class="mx-auto" max-width="900">
-      <v-slide-group
-        v-model="model"
-        class="pa-4"
-        mandatory
-        show-arrows
-        prev-icon="mdi-chevron-left-circle-outline"
-        next-icon="mdi-chevron-right-circle-outline"
-      >
-        <v-slide-item v-for="n in events" :key="n.key">
-          <v-card class="ma-2" width="300">
-            <v-card-text>
-              <p class="text-h4 text--primary">
-                {{ n.name }}
-              </p>
-              <v-col class="text--primary">
-                <v-row>
-                  วันที่: {{ general.displayDate(n.start, true, false) }}
-                </v-row>
-                <v-row>
-                  เวลา: {{ general.displayDate(n.start, false, true) }} -
-                  {{ general.displayDate(n.end, false, true) }}
-                </v-row>
-              </v-col>
-            </v-card-text>
-            <v-card-actions>
-              <v-btn color="error">
-                ยกเลิกการจอง
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-slide-item>
-      </v-slide-group>
-    </v-sheet>
-  </div>
+  <v-container fill-height class="pa-0">
+    <!-- <v-row no-gutters>
+      <v-col>
+        <v-slide-group
+          v-model="model"
+          class="mx-auto"
+          mandatory
+          show-arrows
+          prev-icon="mdi-chevron-left-circle-outline"
+          next-icon="mdi-chevron-right-circle-outline"
+        >
+          <v-slide-item v-for="n in events" :key="n.key">
+            <v-card class="ma-2" width="220">
+              <v-card-title :class="`${n.color}`">
+                <strong style="color:white">{{ n.name }}</strong>
+              </v-card-title>
+              <v-divider></v-divider>
+              <v-card-text>
+                <v-col class="text--primary" style="font-size:15px">
+                  <v-row>
+                    <p>
+                      <strong>วันที่:</strong>
+                      {{ general.displayDate(n.start, true, false) }}
+                    </p>
+                  </v-row>
+                  <v-row>
+                    <p>
+                      <strong>เวลา:</strong>
+                      {{ general.displayDate(n.start, false, true) }} -
+                      {{ general.displayDate(n.end, false, true) }}
+                    </p>
+                  </v-row>
+                </v-col>
+              </v-card-text>
+              <v-divider></v-divider>
+              <v-card-actions>
+                <v-btn color="error">
+                  ยกเลิกการจอง
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-slide-item>
+        </v-slide-group>
+      </v-col>
+    </v-row> -->
+
+    <v-row>
+      <v-col>
+        <div>จำนวนการจอง: {{ events.length }}</div>
+        <v-row no-gutters>
+          <v-col>
+            <v-slide-group
+              v-model="model"
+              class="mx-auto"
+              mandatory
+              show-arrows
+              prev-icon="mdi-chevron-left-circle-outline"
+              next-icon="mdi-chevron-right-circle-outline"
+            >
+              <v-slide-item v-for="n in events" :key="n.key">
+                <v-card class="ma-2" width="220">
+                  <v-card-title :class="`${n.color}`">
+                    <strong style="color:white">{{ n.name }}</strong>
+                  </v-card-title>
+                  <v-divider></v-divider>
+                  <v-card-text>
+                    <v-col class="text--primary" style="font-size:15px">
+                      <v-row>
+                        <p>
+                          <strong>วันที่:</strong>
+                          {{ general.displayDate(n.start, true, false) }}
+                        </p>
+                      </v-row>
+                      <v-row>
+                        <p>
+                          <strong>เวลา:</strong>
+                          {{ general.displayDate(n.start, false, true) }} -
+                          {{ general.displayDate(n.end, false, true) }}
+                        </p>
+                      </v-row>
+                    </v-col>
+                  </v-card-text>
+                  <v-divider></v-divider>
+                  <v-card-actions>
+                    <v-btn color="error" @click="deleteReserve(n)">
+                      ยกเลิกการจอง
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-slide-item>
+            </v-slide-group>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -87,7 +147,6 @@ export default {
         }
 
         this.events = events;
-        console.log(this.events);
       });
     }
   }

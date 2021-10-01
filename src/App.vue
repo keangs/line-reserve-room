e1686eb1071996b6b293d8617195b9c9fc9d47ac
Theerawat <theerawat.a@cdg.co.th>
2021-10-01 15:43:03 +0700

    commit

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <Calendar v-if="action == 'reserve'" />
       <Check v-if="action == 'check'" />
     </v-main>
-    <div style="font-size:12px">v 1.0.1</div>
+    <div style="font-size:12px">v 1.0.2</div>
   </v-app>
 </template>
 
